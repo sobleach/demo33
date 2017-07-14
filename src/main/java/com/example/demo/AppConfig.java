d8267a18ea5130aa0ab54d3f88d290b479549d3b
sobleach <suchat.nu@gmail.com>
2017-07-14 10:59:33 +0700

    Configuration Properties by create class config and binding config value from YAML file.

---
+++ src/main/java/com/example/demo/AppConfig.java
@@ -0,0 +1,85 @@
+package com.example.demo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Thum on 7/14/2017 AD.
+ */
+
+@Component
+@ConfigurationProperties(prefix = "myApp")
+public class AppConfig {
+
+    private String comming;
+    private String car;
+    private String salary;
+    private final Phone phone = new Phone();
+
+    public String getComming() {
+        return comming;
+    }
+
+    public void setComming(String comming) {
+        this.comming = comming;
+    }
+
+    public String getCar() {
+        return car;
+    }
+
+    public void setCar(String car) {
+        this.car = car;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    public Phone getPhone() {
+        return phone;
+    }
+
+    static class Phone{
+        private String smart;
+        private String feature;
+
+        public String getSmart() {
+            return smart;
+        }
+
+        public void setSmart(String smart) {
+            this.smart = smart;
+        }
+
+        public String getFeature() {
+            return feature;
+        }
+
+        public void setFeature(String feature) {
+            this.feature = feature;
+        }
+
+        @Override
+        public String toString() {
+            return "Phone{" +
+                    "smart='" + smart + '\'' +
+                    ", feature='" + feature + '\'' +
+                    '}';
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "AppConfig{" +
+                "comming='" + comming + '\'' +
+                ", car='" + car + '\'' +
+                ", salary='" + salary + '\'' +
+                ", phone=" + phone +
+                '}';
+    }
+}
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -9,6 +10,7 @@ import java.util.Arrays;
 
 @ComponentScan("com")
 @SpringBootApplication
+@EnableConfigurationProperties
 public class DemoApplication {
 
     public static void main(String[] args) {
@@ -19,5 +21,8 @@ public class DemoApplication {
         for(String name : beanName){
             System.out.println(name);
         }
+
+        AppConfig config = (AppConfig) ctx.getBean(AppConfig.class);
+        System.out.println(config.toString());
     }
 }
